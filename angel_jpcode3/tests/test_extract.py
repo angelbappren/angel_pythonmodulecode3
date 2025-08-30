388f3b6aa21adcffac89cb9179e115ac2d6413e2
angelbappren <angelbondocapprenticeship@gmail.com>
2025-08-30 16:54:38 +0100

    multiple revision and correct of test extract - working now under pytest

--- angel_jpcode3/tests/test_extract.py
+++ angel_jpcode3/tests/test_extract.py
@@ -1,22 +1,24 @@
-from extract import extract_source:
+from extract import extract_source
 import pandas as pd
 
-def test_extract_source_csv_working(tempo_path):
-    csv_file = tempo_path / "sample.csv"
-    csv_file.write_text("RequestID,AllocatedUser\1234,Amy\5678\John\n")
-                        
+def test_extract_source_csv_working(tmp_path):
+    
+    csv_file = tmp_path / "sample.csv"
+    csv_file.write_text("RequestID,AllocatedUser\n1234,Amy\n5678,John\n")
+
     df = extract_source(str(csv_file), file_type="csv")
 
     assert not df.empty
-    assert_list(df.columns) == ["RequestID", "AllocatedUser"]
+    assert list(df.columns) == ["RequestID", "AllocatedUser"]
     assert len(df) == 2
 
-
-def test_extract_source_excel_working(tempo_path):
-
-    import pandas as pd
-    excel_path = tempo_path / "trial.xlsx"
-    pd.DataFrame({"ID": [1,2], "Name": ["Amy, John"]}).to_excel(excel_path, index=False)
+def test_extract_source_excel_working(tmp_path):
+    
+    excel_path = tmp_path / "trial.xlsx"
+    pd.DataFrame({
+        "RequestID": [1234, 5678],
+        "AllocatedUser": ["Amy", "John"]
+    }).to_excel(excel_path, index=False)
 
     df = extract_source(str(excel_path), file_type="excel")
 
@@ -24,8 +26,7 @@ def test_extract_source_excel_working(tempo_path):
     assert list(df.columns) == ["RequestID", "AllocatedUser"]
     assert len(df) == 2
 
-
-def test_extract_source_csv_unsupported():
-
-    df = extract_source("unlisted_path", file_type="json")
+def test_extract_source_unsupported_file_type():
+    
+    df = extract_source("fake_path.json", file_type="json")
     assert df.empty

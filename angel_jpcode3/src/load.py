7cc805fe3a6016e140bec23caae2f2700eda971a
angelbappren <angelbondocapprenticeship@gmail.com>
2025-08-31 11:34:23 +0100

    relocated my load func in the correct folder src

--- angel_jpcode3/src/load.py
+++ angel_jpcode3/src/load.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import logging
+import logging.config
 from pathlib import Path
+import sys
+
+config_path = Path(__file__).resolve().parent.parent / "logging.ini"
+logging.config.fileConfig(config_path, defaults={"logfilename": "app.log"})
 
 
 def load_for_csv(df: pd.DataFrame, output_path) -> str:
@@ -9,9 +14,9 @@ def load_for_csv(df: pd.DataFrame, output_path) -> str:
         output_path.parent.mkdir(parents=True, exist_ok=True)
         df.to_csv(output_path, index=False)
 
-        logging.info(f"DataFrame with {len(df)} rows written to '{output_path}'.")
+        logging.info(f"DataFrame with TESTING THIS {len(df)} rows written to '{output_path}'.")
         return str(output_path)
 
     except Exception as save_summary_error:
-        logging.error(f"Failed to save DataFrame to '{output_path}': {str(save_summary_error)}")
+        logging.error(f"Failed to save DataFrame to TESTING THIS'{output_path}': {str(save_summary_error)}")
         return ""
